afa9c4ce61d6f686f8da217997a45e8dce5a17bb
carlossr404 <carlossr404@gmail.com>
2024-06-13 21:03:48 -0500

    added more info to the publications section and continued with CSS changes

--- carlossr404/src/App.js
+++ carlossr404/src/App.js
@@ -34,29 +34,31 @@ function App() {
       <Banner id="home"/>
       
       <p>
-        <h2 className='h2'>Bio</h2>
-        <div>This is a small bio about me (Carlos)</div>
+        <h2 className='h2'>About</h2>
+        <div className='about'>This is a small bio about me (Carlos)</div>
       </p>
 
       <p>
         <h2 id="exp" className='h2'>Experience</h2>
         <div className='experience-block'>
           <Experience 
-          logo={'osh_logo.jpeg'}
+          logo={'imgs/osh_logo.jpeg'}
           title={"Clinical Information Specialist"} 
           company={"Oak Street Health"}
           location={"Houston, TX"}
           date={"August 2023 - Present"}
           desc={desc_CIS} />
+
           <Experience
-          logo={'rhodes_logo.jpeg'}
+          logo={'imgs/rhodes_logo.jpeg'}
           title={"MOVE-IT Lab Coordinator"}
           company={"Rhodes College"}
           location={"Memphis, TN"}
           date={"August 2022 - May 2023"}
           desc={desc_MOVEIT} />
+
           <Experience
-          logo={'vandy_logo.jpeg'}
+          logo={'imgs/vandy_logo.jpeg'}
           title={"Visiting Researcher"}
           company={"Vanderbilt University"}
           location={"Nashville, TN"}
@@ -72,11 +74,20 @@ function App() {
 
       <p>
         <h2 id="pubs" className='h2'>Publications</h2>
-        <div>
+        <div className='pub'>
           <a href='https://dl.acm.org/doi/abs/10.1145/3449067'>Estimating Distance in Action Space in Augmented Reality</a>
+          <br/>
+          <text>Holly C. Gagnon, <u>Carlos Salas Rosales</u>, Ryan Mileris, Jeanine K. Stefanucci, Sarah H. Creem-Regehr, and Robert E. Bodenheimer</text>
+          <br/>
+          <text>ACM Transactions on Applied Perception | <a href='docs/2021_Gagnon_ACM_Estimating.pdf'>PDF</a></text>
+          <br/>
         </div>
-        <div>
+        <div className='pub'>
           <a href='https://ieeexplore.ieee.org/abstract/document/8798095'>Distance Judgments to On- and Off Ground Objects in Augmented Reality</a>
+          <br/>
+          <text><u>Carlos Salas Rosales</u>, Grant Pointon, Haley Adams, Jeanine Stefanucci, Sarah Creem-Regehr, William B. Thompson, and Bobby Bodenheimer</text>
+          <br/>
+          <text>IEEE Virtual Reality 2019 | <a href='docs/2019_SalasRosales_IEEEVR_Distance.pdf'>PDF</a></text>
         </div>  
       </p>
 
